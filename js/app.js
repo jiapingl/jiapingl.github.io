e0e0389e44eb6b5da66cab6f73c13a1b306b75fe
jiapingl <luojp@luodeMacBook-Pro.local>
2018-01-13 15:16:53 -0800

    none

--- js/app.js
+++ js/app.js
@@ -23,25 +23,29 @@ $(document).ready(function(){
 		var target;
 
 		if(cur>=0&&cur<=sec1){
-			target=$("#menu li:nth-child(1) a");
+			target=$("#menu li:nth-child(1)");
 		}
 		else if(cur<=sec2&&cur>sec1){
-			target=$("#menu li:nth-child(2) a");
+			target=$("#menu li:nth-child(2)");
 		}
 		else if(cur>sec2&&cur<=sec3){
-			target=$("#menu li:nth-child(3) a");
+			target=$("#menu li:nth-child(3)");
 		}
 		else if(cur>sec3&&cur<sec4){
-			target=$("#menu li:nth-child(4) a");
+			target=$("#menu li:nth-child(4)");
 		}
 		else if(cur>=sec4){
-			target=$("#menu li:nth-child(5) a");
+			target=$("#menu li:nth-child(5)");
 		}
 
 		target.addClass("active");
-		$("#menu li a").not(target).removeClass("active");
+		$("#menu li").not(target).removeClass("active");
 
 	})
+
+	$('#menu li a').click(function(){
+    	$('.navbar-collapse').collapse('hide');
+	});
 
 })
 
